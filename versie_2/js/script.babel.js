71a4b31092fe3fd275f2496d219fae3b7336fdb4
JeffreyKoning <44468480+JeffreyKoning@users.noreply.github.com>
2019-02-03 15:26:41 +0100

    Add files via upload

---
+++ versie_2/js/script.babel.js
@@ -0,0 +1,473 @@
+'use strict';
+//Aanpassen volume achtergrondmuziek
+var audio = document.getElementById("bgAudio");
+audio.volume = 0.1;
+
+var goed_antwoord_sound = new Audio('sound/good.mp3');
+var fout_antwoord_sound = new Audio('sound/wrong.mp3');
+
+var afbeelding1;
+//Input field 1-----------------------------------------------------
+function input1() {
+  //Inputfield
+  var eennameField = document.getElementById('nameField1').value;
+  var resultaat_een = document.getElementById('result1');
+
+  //Input tekst in variabel plaatsen
+  afbeelding1 = eennameField
+  console.log(afbeelding1)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[0].img = afbeelding1
+  console.log(cardsArray[0])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input1, false);
+  
+//Input field 1-----------------------------------------------------
+
+var afbeelding2;
+//Input field 2-----------------------------------------------------
+function input2() {
+  //Inputfield
+  var tweedeField = document.getElementById('nameField2').value;
+  var resultaat_twee = document.getElementById('result2');
+
+  //Input tekst in variabel plaatsen
+  afbeelding2 = tweedeField
+  console.log(afbeelding2)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[1].img = afbeelding2
+  console.log(cardsArray[1])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input2, false);
+  
+//Input field 2-----------------------------------------------------
+
+var afbeelding3;
+//Input field 3-----------------------------------------------------
+function input3() {
+  //Inputfield
+  var derdeField = document.getElementById('nameField3').value;
+  var resultaat_drie = document.getElementById('result3');
+
+  //Input tekst in variabel plaatsen
+  afbeelding3 = derdeField
+  console.log(afbeelding3)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[2].img = afbeelding3
+  console.log(cardsArray[2])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input3, false);
+  
+//Input field 3-----------------------------------------------------
+
+var afbeelding4;
+//Input field 4-----------------------------------------------------
+function input4() {
+  //Inputfield
+  var vierdeField = document.getElementById('nameField4').value;
+  var resultaat_vier = document.getElementById('result4');
+
+  //Input tekst in variabel plaatsen
+  afbeelding4 = vierdeField
+  console.log(afbeelding4)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[3].img = afbeelding4
+  console.log(cardsArray[3])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input4, false);
+  
+//Input field 4------------------------------------------------------
+
+var afbeelding5;
+//Input field 5-----------------------------------------------------
+function input5() {
+  //Inputfield
+  var vijfdeField = document.getElementById('nameField5').value;
+  var resultaat_vijf = document.getElementById('result5');
+
+  //Input tekst in variabel plaatsen
+  afbeelding5 = vijfdeField
+  console.log(afbeelding5)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[4].img = afbeelding5
+  console.log(cardsArray[4])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input5, false);
+  
+//Input field 5------------------------------------------------------
+
+var afbeelding6;
+//Input field 6-----------------------------------------------------
+function input6() {
+  //Inputfield
+  var zesdeField = document.getElementById('nameField6').value;
+  var resultaat_zes = document.getElementById('result6');
+
+  //Input tekst in variabel plaatsen
+  afbeelding6 = zesdeField
+  console.log(afbeelding6)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[5].img = afbeelding6
+  console.log(cardsArray[5])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input6, false);
+  
+//Input field 6------------------------------------------------------
+
+var afbeelding7;
+//Input field 7-----------------------------------------------------
+function input7() {
+  //Inputfield
+  var zevendeField = document.getElementById('nameField7').value;
+  var resultaat_zeven = document.getElementById('result7');
+
+  //Input tekst in variabel plaatsen
+  afbeelding7 = zevendeField
+  console.log(afbeelding7)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[6].img = afbeelding7
+  console.log(cardsArray[6])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input7, false);
+  
+//Input field 7------------------------------------------------------
+
+var afbeelding8;
+//Input field 8-----------------------------------------------------
+function input8() {
+  //Inputfield
+  var achtsteField = document.getElementById('nameField8').value;
+  var resultaat_acht = document.getElementById('result8');
+
+  //Input tekst in variabel plaatsen
+  afbeelding8 = achtsteField
+  console.log(afbeelding8)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[7].img = afbeelding8
+  console.log(cardsArray[7])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input8, false);
+  
+//Input field 8------------------------------------------------------
+
+var afbeelding9;
+//Input field 9-----------------------------------------------------
+function input9() {
+  //Inputfield
+  var negendeField = document.getElementById('nameField9').value;
+  var resultaat_negen = document.getElementById('result9');
+
+  //Input tekst in variabel plaatsen
+  afbeelding9 = negendeField
+  console.log(afbeelding9)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[8].img = afbeelding9
+  console.log(cardsArray[8])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input9, false);
+  
+//Input field 9------------------------------------------------------
+
+var afbeelding10;
+//Input field 10-----------------------------------------------------
+function input10() {
+  //Inputfield
+  var tiendeField = document.getElementById('nameField10').value;
+  var resultaat_tien = document.getElementById('result10');
+
+  //Input tekst in variabel plaatsen
+  afbeelding10 = tiendeField
+  console.log(afbeelding10)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[9].img = afbeelding10
+  console.log(cardsArray[9])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input10, false);
+  
+//Input field 10------------------------------------------------------
+
+var afbeelding11;
+//Input field 11-----------------------------------------------------
+function input11() {
+  //Inputfield
+  var elfdeField = document.getElementById('nameField11').value;
+  var resultaat_elf = document.getElementById('result11');
+
+  //Input tekst in variabel plaatsen
+  afbeelding11 = elfdeField
+  console.log(afbeelding11)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[10].img = afbeelding11
+  console.log(cardsArray[10])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input11, false);
+  
+//Input field 11------------------------------------------------------
+
+var afbeelding12;
+//Input field 12-----------------------------------------------------
+function input12() {
+  //Inputfield
+  var twaalfdeField = document.getElementById('nameField12').value;
+  var resultaat_twaalf = document.getElementById('result12');
+
+  //Input tekst in variabel plaatsen
+  afbeelding12 = twaalfdeField
+  console.log(afbeelding12)
+  
+  //Ingevoerde URL toevoegen aan variabel
+  cardsArray[11].img = afbeelding12
+  console.log(cardsArray[11])
+
+}
+  //Eventlisterner voor button
+  var knopklik = document.getElementById('subButton');
+  knopklik.addEventListener('click', input12, false);
+  
+//Input field 12------------------------------------------------------
+
+//Array met afbeeldingen van kaarten
+var cardsArray = [{
+  'name': 'foto1',
+  'img': 'img/spanje.png'
+}, {
+  'name': 'foto2',
+  'img': 'img/belgie.png'
+}, {
+  'name': 'foto3',
+  'img': 'img/duitsland.png'
+}, {
+  'name': 'foto4',
+  'img': 'img/finland.png'
+}, {
+  'name': 'foto5',
+  'img': 'img/frankrijk.png'
+}, {
+  'name': 'foto6',
+  'img': 'img/italie.png'
+}, {
+  'name': 'foto7',
+  'img': 'img/nederland.png'
+}, {
+  'name': 'foto8',
+  'img': 'img/oostenrijk.png'
+}, {
+  'name': 'foto9',
+  'img': 'img/portugal.png'
+}, {
+  'name': 'foto10',
+  'img': 'img/roemenie.png'
+}, {
+  'name': 'foto11',
+  'img': 'img/rusland.png'
+}, {
+  'name': 'foto12',
+  'img': 'img/zweden.png'
+}];
+
+
+//Dupliceren van kaarten en zorg ervoor dat deze kaarten in willekeurige volgorde worden weergegeven
+var gameGrid = cardsArray.concat(cardsArray).sort(function () {
+  return 0.5 - Math.random();
+});
+
+//Opslaan van eerste gok
+var firstGuess = '';
+//Opslaan van tweede gok
+var secondGuess = '';
+//Tellen van aantal geselecteerde items
+var count = 0;
+//Voorkomen dat zelfde twee keer element geselecteerd wordt
+var previousTarget = null;
+//Delay zodat speler de geselecteerde kaart kan zien voordat deze omdraait
+var delay = 1200;
+
+var game = document.getElementById('game');
+var grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+gameGrid.forEach(function (item) {
+  var name = item.name,
+      img = item.img;
+
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  var front = document.createElement('div');
+  front.classList.add('front');
+
+  var back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = 'url(' + img + ')';
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+  
+});
+
+
+//Score counter
+var score = 0;
+
+//Naam persoon opvragen na afronding spel
+function naam_opvragen() {
+  var person = prompt("Please enter your name", "");}
+
+//Match functie
+var match = function match() {
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.add('match');
+    //Tellen van aantal weggespeelde kaarten
+    score ++
+    //Weergeven van score
+    document.getElementById("score").innerHTML = "Punten: " + score;
+    
+;
+//Einde spel afbeelding en bericht + Optie om opnieuw te beginnen
+if (score == 24)
+  {document.getElementById("game").innerHTML = "<img src='/img/goedgedaan.png' width='636' height='316' >" + 'Heel goed gedaan.' +"<a href='index.html' span style='color: white;' >Speel nog een keer</a>"}
+    });
+  };
+
+//Weergeven van score
+document.getElementById("score").innerHTML = "Punten: " + score;
+
+//Functie voor het resetten van geselecteerde kaarten na twee clicks
+var resetGuesses = function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.remove('selected');
+  });
+};
+
+//Eventlistener toevoegen aan grid
+grid.addEventListener('click', function (event) {
+
+  var clicked = event.target;
+
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
+    return;
+  }
+
+//Toestaan dat 'selected' wordt toegepast op maximaal 2 kaarten 
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      //Toewijzen eerste gok
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      //Toewijzen tweede gok
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+//Wanneer de gebruiker twee keuzes heeft opgegeven
+    if (firstGuess && secondGuess) {
+      //En deze keuzes matchen
+      if (firstGuess === secondGuess) {
+        //Run de match functie
+        setTimeout(match, delay, goed_antwoord_sound.play());}
+      if (firstGuess !== secondGuess){
+      fout_antwoord_sound.play();}
+  
+
+      setTimeout(resetGuesses, delay);
+
+
+    }
+    //Set previous target to clicked
+    previousTarget = clicked;
+  }
+});
+
+//Stopwatch
+var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+//Format van stopwatch
+function tijd() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+  
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(tijd, 1000);
+}
+timer();
+
+
+//Start knop
+start.onclick = timer;
+
+//Stop knop
+stop.onclick = function() {
+    clearTimeout(t);
+}
